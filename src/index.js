1db9c1b0092f8fd1fa282c20c9ee7a6fba98a211
Steven Smith <steven.76484@gmail.com>
2022-04-24 15:06:42 -0400

    add rxjs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello, world!");
+import { from } from "rxjs";
+import { map } from "rxjs/operators";
+
+const adEls = Array.from(document.querySelectorAll("#Ads li"));
+
+from(adEls)
+  .pipe(
+    map((adEl, index) => ({
+      index,
+      text: adEl.innerText,
+    }))
+  )
+  .subscribe((ad) => console.log(ad));
