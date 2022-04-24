8bbb2ea9125f92f989850a4f96d32bbf3aa1e2a9
Steven Smith <steven.76484@gmail.com>
2022-04-24 15:16:46 -0400

    mergeMap ad els to intersection observer events

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import { from } from "rxjs";
-import { map } from "rxjs/operators";
+import { from, Observable } from "rxjs";
+import { mergeMap } from "rxjs/operators";
+
+const fromAdEl = (el, index) => {
+  const text = el.innerText;
+
+  return new Observable((observer) => {
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+
+        observer.next({
+          index,
+          isInersecting: entry.isIntersecting,
+          text,
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    intersectionObserver.observe(el);
+
+    return () => {
+      intersectionObserver.unobserve(el);
+    };
+  });
+};
 
 const adEls = Array.from(document.querySelectorAll("#Ads li"));
 
 from(adEls)
-  .pipe(
-    map((adEl, index) => ({
-      index,
-      text: adEl.innerText,
-    }))
-  )
+  .pipe(mergeMap((adEl, index) => fromAdEl(adEl, index)))
   .subscribe((ad) => console.log(ad));
